410a0ee26d0929188909752a97a64d0d33fc62e3
MOBOLA MORONTO <t2wycemo@gmail.com>
2021-02-24 12:08:41 +0000

    Automatically backed up by Learn

--- src/components/FillerText.js
+++ src/components/FillerText.js
@@ -4,7 +4,7 @@ export default class FillerText extends React.Component {
   render() {
     return (
       <p>
-        I am a filler text. I can be usedti fill your screen. Amazing!
+        I am a filler text. I can be used to fill your screen. Amazing!
       </p>
       )
   }
